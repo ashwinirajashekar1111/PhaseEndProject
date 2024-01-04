dc21b19cc77ff3124d19ba2024eb2d701d374110
ashwinirajashekar1111 <150044218+ashwinirajashekar1111@users.noreply.github.com>
2024-01-04 12:51:19 +0530

    Add files via upload

---
+++ WebApplication2/Views/DeptMaster.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication2.Views
+{
+    // DeptMaster.cs
+    public class DeptMaster
+    {
+        public int DeptCode { get; set; }
+        public string DeptName { get; set; }
+        public virtual ICollection<EmpProfile> EmpProfiles { get; set; }
+    }
+
+    // EmpProfile.cs
+    public class EmpProfile
+    {
+        public int EmpCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public int DeptCode { get; set; }
+        public virtual DeptMaster DeptMaster { get; set; }
+    }
+    
+}
+
+    
